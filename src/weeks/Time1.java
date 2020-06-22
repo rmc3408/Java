0f9261a3daf9ba1a4b6d3950476ec25a0000b2d3
rmc3408 <rmc3408@protonmail.com>
2020-06-22 13:57:22 -0400

    Week4

---
+++ src/weeks/Time1.java
@@ -0,0 +1,38 @@
+package weeks;
+
+public class Time1 {
+
+	private int hour;
+	private int minute;
+	private int second;
+	
+	public void setTime(int h, int m, int s) 
+	{
+		
+		if (h < 0 || h >= 24 || m < 0 || m >= 60 || s < 0 || s >= 60) 
+		{
+			throw new IllegalArgumentException("time out of range!");
+		}
+		else 
+		{
+			this.hour = h;
+			this.minute = m;
+			this.second = s;
+		}
+		
+	}
+	public String ToUniversal() 
+	{
+		return String.format("%02d:%02d:%02d", hour, minute, second);
+		
+	}
+	public String ToString()
+	{
+	return String.format("%d:%02d:%02d %s", ((hour == 0 || hour == 12) ? 12 : hour % 12),
+	minute, second, (hour < 12 ? "AM" : "PM"));
+	}
+
+	
+	
+	
+}
---
+++ src/weeks/Time1Main.java
@@ -0,0 +1,37 @@
+package weeks;
+
+public class Time1Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Time1 myTime = new Time1();
+		
+		
+		displayTime("After time object is created", myTime);
+		System.out.println();
+
+		
+		myTime.setTime(11, 18, 36);
+		displayTime("After calling setTime", myTime);
+		System.out.println();
+
+		try
+		{
+		myTime.setTime(99, 99, 99); // all values out of range
+		}
+		catch (IllegalArgumentException e)
+		{
+		System.out.printf("Exception: %s%n%n", e.getMessage());
+		}
+
+
+		
+	}
+	private static void displayTime(String header, Time1 t)
+	{
+	System.out.printf("%s%nUniversalTime: %s%nStandardTime: %s%n",
+	header, t.ToUniversal(), t.ToString());
+	}
+
+}
