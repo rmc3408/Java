32764421f557a9e1c768295c943545a14095e3ff
rmc3408 <rmc3408@protonmail.com>
2020-06-27 13:24:58 -0400

    java A2

--- src/Assigment/A2Q1.java
+++ src/Assigment/A2Q1.java
@@ -1,48 +1,47 @@
 package Assigment;
 
-import java.util.InputMismatchException;
+
 import java.util.Scanner;
 
 public class A2Q1 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
 		System.out.print("Please, enter number: ");
-		
-		try{
+
+		try {
 			int numUser = input.nextInt();
-			if(numUser < 0){
-				throw new InputMismatchException("Enter positive whole number");
+			input.close();
+
+			if (numUser < 0) {
+				throw new NumberFormatException("Must be positive integer");
 
 			}
-			input.close();
 			System.out.print(revertNum(numUser));
-			
-		}catch(InputMismatchException e){
-			//System.out.println("Please input a positive whole number.");
+
+		} catch (NumberFormatException e) {
 			System.out.println(e.getMessage());
+		} catch (Exception ex) {
+			System.out.println("Invalid character");
 		}
-		
+
 	}
-	public static int revertNum(int num) 
-	{
+	public static int revertNum(int num) {
 		int count = 1;
 		int numForward = num;
-		while (num / 10 >= 1) 
-		{
-            num = num / 10;
-            count++;
+		while (num / 10 >= 1) {
+			num = num / 10;
+			count++;
 		}
 		int unit = 0;
-				
-		for (int i = count; i > 0;i--) 
-		{
-			unit = unit + (((int)Math.pow(10, i - 1)) * (numForward % 10));
+
+		for (int i = count; i > 0; i--) {
+			unit = unit + (((int) Math.pow(10, i - 1)) * (numForward % 10));
 			numForward = numForward / 10;
 		}
-		
+
 		return unit;
 	}
-	
+
 }
