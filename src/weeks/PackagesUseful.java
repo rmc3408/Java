88e55d0e5843fe051705d87598d0f06f7bfe0022
rmc3408 <rmc3408@protonmail.com>
2020-06-29 11:48:23 -0400

    import static

--- src/weeks/PackagesUseful.java
+++ src/weeks/PackagesUseful.java
@@ -9,7 +9,15 @@ package weeks;
     import java.lang;
 
 */
+// You can import static class !
+import static java.lang.Math.E;
 
 public class PackagesUseful {
     
+    public static void main(String[] args)
+    {
+        System.out.printf("e = %f", E);
+
+    }
+
 }
