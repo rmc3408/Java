aa8c7944357f678a2a8322e919b63a9594dc3b8f
rmc3408 <rmc3408@protonmail.com>
2020-07-10 11:52:10 -0400

    p

--- src/Udemy/Testing.java
+++ src/Udemy/Testing.java
@@ -5,10 +5,7 @@ public class Testing {
 
     public static void main(String[] args)
     {
-        int c = 2;
-        int val = c > 1 ? 5 : 0;
-        System.out.println(val);
-
+        
 
     }
 }
