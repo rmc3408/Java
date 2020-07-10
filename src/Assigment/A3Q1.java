d6133cd1c08d7d082fead38fca11fd5d8d8f983d
rmc3408 <rmc3408@protonmail.com>
2020-07-10 13:01:35 -0400

    a3

---
+++ src/Assigment/A3Q1.java
@@ -0,0 +1,14 @@
+package Assigment;
+
+/*
+Create a class Rectangle with attributes length and width, 
+each of which defaults
+to 1. Provide methods that calculate the rectangle’s perimeter and area. It has set and
+get methods for both length and width. The set methods should verify that length
+and width are each floating-point number larger than 0.0 and less than 20.0.
+*/
+public class A3Q1 {
+    
+
+
+}
