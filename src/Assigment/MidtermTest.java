102d96234e925107660c96c29ac39fd9146aca12
rmc3408 <rmc3408@protonmail.com>
2020-07-13 12:48:55 -0400

    r

---
+++ src/Assigment/MidtermTest.java
@@ -0,0 +1,33 @@
+package Assigment;
+
+public class MidtermTest {
+
+    public static void main(String[] args) {
+
+        // Contructor
+        MyDay time1 = new MyDay("Mon");
+
+        // C - Return the day using get method
+        System.out.printf("the variable stored is %s%n", time1.getDay());
+
+        // B - Print the day
+        time1.printDay();
+
+        // D - Return the next day
+        System.out.printf("The next day is %s%n", time1.nextDay());
+
+        // E - Return the Previous day
+        System.out.printf("The Previous day is %s%n", time1.previousDay());
+
+        System.out.println("------------------");
+
+        // A - set Day
+        time1.setDay("fri");
+
+        // Print the day, next day and previous day 
+        time1.printDay();
+        System.out.printf("The next day is %s%n", time1.nextDay());
+        System.out.printf("The Previous day is %s%n", time1.previousDay());
+
+    }
+}
---
+++ src/Assigment/MyDay.java
@@ -0,0 +1,124 @@
+package Assigment;
+
+public class MyDay {
+
+    protected String day;
+
+    //Set method 
+    public void setDay(String d) {
+        this.day = d;
+    }
+
+    //Get and return the day method 
+    public String getDay() {
+        return day;
+    }
+
+
+    //Constructor
+    public MyDay(String dayWeek) {
+        this.day = dayWeek;
+    }
+
+    // Print a Day method
+    public void printDay() {
+
+        String d = getDay();
+
+        switch (d.toLowerCase()) {
+            case "sun":
+                System.out.println("The day is Sunday");
+                break;
+            case "mon":
+                System.out.println("The day is Monday");
+                break;
+            case "tue":
+                System.out.println("The day is Tuesday");
+                break;
+            case "wed":
+                System.out.println("The day is Wednesday");
+                break;
+            case "thr":
+                System.out.println("The day is Thrusday");
+                break;
+            case "fri":
+                System.out.println("The day is Friday");
+                break;
+            case "sat":
+                System.out.println("The day is Saturday");
+                break;
+            default:
+                System.out.println("You did enter a valid day");
+                break;
+        }
+    }
+
+    public String nextDay() {
+
+        String d = getDay();
+
+        String next;
+        switch (d.toLowerCase()) {
+            case "sun":
+                next = "Monday";
+                break;
+            case "mon":
+                next = "Tueday";
+                break;
+            case "tue":
+                next = "Wednesday";
+                break;
+            case "wed":
+                next = "Thrusday";
+                break;
+            case "thr":
+                next = "Friday";
+                break;
+            case "fri":
+                next = "Saturday";
+                break;
+            case "sat":
+                next = "Sunday";
+                break;
+            default:
+                next = "Invalid day";
+                break;
+        }
+        return next;
+    }
+
+    public String previousDay() {
+
+        String d = getDay();
+        String previous;
+
+        switch (d.toLowerCase()) {
+            case "sun":
+                previous = "Saturday";
+                break;
+            case "mon":
+                previous = "Sunday";
+                break;
+            case "tue":
+                previous = "Monday";
+                break;
+            case "wed":
+                previous = "Tuesday";
+                break;
+            case "thr":
+                previous = "Wednesday";
+                break;
+            case "fri":
+                previous = "Thrusday";
+                break;
+            case "sat":
+                previous = "Friday";
+                break;
+            default:
+                previous = "Invalid day";
+                break;
+        }
+        return previous;
+    }
+
+}
