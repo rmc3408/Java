19591835cea7ed144b5db8f3714284e08890bbfd
rmc3408 <rmc3408@protonmail.com>
2020-07-20 10:22:10 -0400

    b

---
+++ src/GUI/src/Week9.java
@@ -0,0 +1,18 @@
+
+
+public class Week9 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.print(3.77);
+		System.out.println();
+		calc(5);
+
+	}
+	public static void calc(double d) {
+		System.out.print(d*d);
+		
+	}
+
+}
---
+++ src/Udemy/Polymorphism/SuperClass.java
@@ -0,0 +1,63 @@
+package Udemy.Polymorphism;
+
+
+// CommissionEmployee class uses methods to manipulate its 
+// private instance variables.
+
+public class SuperClass
+{
+   
+   private double grossSales; // gross weekly sales       
+   private double commissionRate; // commission percentage
+
+   // five-argument constructor
+   public SuperClass(double grossSales, 
+      double commissionRate)
+   {
+      
+      this.grossSales = grossSales;
+      this.commissionRate = commissionRate;
+   } 
+
+   // set gross sales amount
+   public void setGrossSales(double grossSales)
+   {
+      
+      this.grossSales = grossSales;
+   } 
+
+   // return gross sales amount
+   public double getGrossSales()
+   {
+      return grossSales;
+   } 
+
+   // set commission rate
+   public void setCommissionRate(double commissionRate)
+   {
+      
+      this.commissionRate = commissionRate;
+   } 
+
+   // return commission rate
+   public double getCommissionRate()
+   {
+      return commissionRate;
+   }
+
+   // calculate earnings
+   public double earnings()
+   {
+      return getCommissionRate() * getGrossSales();
+   } 
+
+   // return String representation of CommissionEmployee object
+   @Override 
+   public String toString()
+   {
+      return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f", 
+         "commission employee",
+         "gross sales", getGrossSales(), 
+         "commission rate", getCommissionRate());
+   } 
+} 
