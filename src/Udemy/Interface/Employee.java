c238490a80c2ae760bfae30e8f597652769a8ccb
rmc3408 <rmc3408@protonmail.com>
2020-07-20 10:31:56 -0400

    b

--- src/Udemy/Interface/Employee.java
+++ src/Udemy/Interface/Employee.java
@@ -6,12 +6,10 @@ package Udemy.Interface;
 public abstract class Employee implements Payable
 {
    private final String firstName;
-   private final String lastName;
-   private final String socialSecurityNumber;
+   
 
    // constructor
-   public Employee(String firstName, String lastName, 
-      String socialSecurityNumber)
+   public Employee(String firstName)
    {
       this.firstName = firstName;
       this.lastName = lastName;
