db2d93fe20065d0eb21dbb02e7759a3b833e3ac3
rmc3408 <rmc3408@protonmail.com>
2020-07-24 09:09:21 -0400

    a

--- src/Udemy/Abstract/SubClass.java
+++ src/Udemy/Abstract/SubClass.java
@@ -3,30 +3,22 @@ package Udemy.Abstract;
 // Fig. 10.8: BasePlusCommissionEmployee.java
 // BasePlusCommissionEmployee class extends CommissionEmployee.
 
-public class BasePlusCommissionEmployee extends CommissionEmployee 
+public class SubClass extends SuperClass 
 {
    private double baseSalary; // base salary per week
 
    // constructor
-   public BasePlusCommissionEmployee(String firstName, String lastName, 
-      String socialSecurityNumber, double grossSales,
+   public SubClass(String firstName, double grossSales,
       double commissionRate, double baseSalary)
    {
-      super(firstName, lastName, socialSecurityNumber, 
-         grossSales, commissionRate);
-
-      if (baseSalary < 0.0) // validate baseSalary                  
-         throw new IllegalArgumentException("Base salary must be >= 0.0");
-            
+      super(firstName, grossSales, commissionRate);
       this.baseSalary = baseSalary;                
    }
 
    // set base salary
    public void setBaseSalary(double baseSalary)
    {
-      if (baseSalary < 0.0) // validate baseSalary                  
-         throw new IllegalArgumentException("Base salary must be >= 0.0");
-            
+       
       this.baseSalary = baseSalary;                
    } 
 
--- src/Udemy/Abstract/SuperClass.java
+++ src/Udemy/Abstract/SuperClass.java
@@ -3,25 +3,16 @@ package Udemy.Abstract;
 // Fig. 10.7: CommissionEmployee.java
 // CommissionEmployee class extends Employee.
 
-public class CommissionEmployee extends Employee 
+public class SuperClass extends AbstractClass 
 {
    private double grossSales; // gross weekly sales
    private double commissionRate; // commission percentage
 
    // constructor
-   public CommissionEmployee(String firstName, String lastName, 
-      String socialSecurityNumber, double grossSales, 
+   public SuperClass(String firstName, double grossSales, 
       double commissionRate)
    {
-      super(firstName, lastName, socialSecurityNumber);
-
-      if (commissionRate <= 0.0 || commissionRate >= 1.0) // validate 
-         throw new IllegalArgumentException(
-            "Commission rate must be > 0.0 and < 1.0");
-
-      if (grossSales < 0.0) // validate
-         throw new IllegalArgumentException("Gross sales must be >= 0.0");
-
+      super(firstName);
       this.grossSales = grossSales;
       this.commissionRate = commissionRate;
    } 
@@ -29,9 +20,7 @@ public class CommissionEmployee extends Employee
    // set gross sales amount
    public void setGrossSales(double grossSales)
    {
-      if (grossSales < 0.0) // validate
-         throw new IllegalArgumentException("Gross sales must be >= 0.0");
-
+      
       this.grossSales = grossSales;
    } 
 
@@ -44,10 +33,6 @@ public class CommissionEmployee extends Employee
    // set commission rate
    public void setCommissionRate(double commissionRate)
    {
-      if (commissionRate <= 0.0 || commissionRate >= 1.0) // validate
-         throw new IllegalArgumentException(
-            "Commission rate must be > 0.0 and < 1.0");
-
       this.commissionRate = commissionRate;
    } 
 
@@ -68,10 +53,10 @@ public class CommissionEmployee extends Employee
    @Override                                                   
    public String toString()                                    
    {                                                           
-      return String.format("%s: %s%n%s: $%,.2f; %s: %.2f",    
+      return String.format("%s: %s%n  %s: $%,.2f; %s: %.2f",    
          "commission employee", super.toString(),              
          "gross sales", getGrossSales(),                       
          "commission rate", getCommissionRate());             
    } 
-} // end class CommissionEmployee
+} 
 
