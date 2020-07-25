dc7662dd72783cbebcf5551e6af0ac871eda3411
rmc3408 <rmc3408@protonmail.com>
2020-07-25 15:39:37 -0400

    b

--- src/GUI/src/FirstFrame.java
+++ src/GUI/src/FirstFrame.java
@@ -1,12 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 public class FirstFrame extends JFrame {
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
